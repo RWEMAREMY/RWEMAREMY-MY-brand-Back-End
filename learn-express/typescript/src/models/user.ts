776736514fc5d999fc8b09a8be85d26d8d8ad1fa
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-02-26 14:38:51 +0200

    admin switched to admin in models/user

--- learn-express/typescript/src/models/user.ts
+++ learn-express/typescript/src/models/user.ts
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        default: "admin",
+        default: "user",
         enum: ['user', 'admin']
     }
 },
