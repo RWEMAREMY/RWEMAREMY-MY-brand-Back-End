18d0934a9a63aed3ccb70dd9a47a325ae601cd26
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-03-14 22:43:55 +0200

    all

--- src/controller/Querries.ts
+++ src/controller/Querries.ts
@@ -44,22 +44,6 @@ export const deleteQuerry = async (req: Request, res: Response) => {
   }
 };
 
-// export const getallQuerry = async (req: Request, res: Response) => {
-//     try {
-//       // const commentid=req.params.id;
-//         const allquerry = await Querry.find();
-//         if(!allquerry){
-//             return res.json({message:"Querry not Found"});
-//         }
-
-//         res.send(allquerry);
-//         const thisquerry = new Querry({content:req.body.content,
-//             email:req.body.email,author:req.body.author  });
-//             await thisquerry.save();
-//     } catch (err) {
-//         res.status(500).json({ message: (err as Error).message });
-//     }
-//   };
 
 export const getSingleQuerry = async (req: Request, res: Response) => {
   try {
